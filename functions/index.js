6521114d1b6e65c4432d5078d11467ab0b815c14
iamandi <aakawade9@gmail.com>
2019-11-20 00:58:27 -0800

    infura integration

--- functions/index.js
+++ functions/index.js
@@ -32,9 +32,36 @@ exports.addWallet = functions.auth.user().onCreate(user => {
   const btcWallet = cw.generateWallet("BTC");
   const ethWallet = cw.generateWallet("ETH");
   const wallet = {
-    bitcoin: { balance: 0, publicAddress: btcWallet.address },
-    ethereum: { balance: 0, publicAddress: ethWallet.address },
-    donpia: { balance: 0, publicAddress: ethWallet.address },
+    wallet: [
+      {
+        rank: 1,
+        ticker: "DON",
+        token: "Donpia",
+        balance: 0,
+        balanceUsd: 0,
+        publicAddress: ethWallet.address,
+        classes: "red"
+      },
+      {
+        rank: 2,
+        ticker: "BTC",
+        token: "Bitcoin",
+        balance: 0,
+        balanceUsd: 0,
+        publicAddress: btcWallet.address,
+        classes: "orange"
+      },
+      {
+        rank: 3,
+        ticker: "ETH",
+        token: "Ethereum",
+        balance: 0,
+        balanceUsd: 0,
+
+        publicAddress: ethWallet.address,
+        classes: "green"
+      }
+    ],
     uid
   };
   const walletPrivate = {
@@ -54,3 +81,75 @@ exports.addWallet = functions.auth.user().onCreate(user => {
 
   return batch.commit();
 });
+/* 
+exports.addWallet = functions.auth.user().onCreate(user => {
+  const uid = user.uid;
+  console.log(`uid: ${uid}`);
+
+  const batch = db.batch();
+
+  const email = user.email; // The email of the user.
+  const displayName = user.displayName ? user.displayName : ""; // The display name of the user.
+  const firstName = user.firstName ? user.firstName : "";
+  const lastName = user.lastName ? user.lastName : "";
+  const phone = user.phone ? user.phone : "";
+  const userProfile = {
+    email,
+    displayName,
+    firstName,
+    lastName,
+    phone
+  };
+
+  const btcWallet = cw.generateWallet("BTC");
+  const ethWallet = cw.generateWallet("ETH");
+  const wallet = {
+    wallet: [
+      {
+        rank: 1,
+        ticker: "DON",
+        token: "Donpia",
+        balance: 0,
+        balanceUsd: 0,
+        publicAddress: ethWallet.address,
+        classes: "red"
+      },
+      {
+        rank: 2,
+        ticker: "BTC",
+        token: "Bitcoin",
+        balance: 0,
+        balanceUsd: 0,
+        publicAddress: btcWallet.address,
+        classes: "orange"
+      },
+      {
+        rank: 3,
+        ticker: "ETH",
+        token: "Ethereum",
+        balance: 0,
+        balanceUsd: 0,
+
+        publicAddress: ethWallet.address,
+        classes: "green"
+      }
+    ],
+    uid
+  };
+  const walletPrivate = {
+    bitcoin: { privateKey: btcWallet.privateKey },
+    ethereum: { privateKey: ethWallet.privateKey },
+    donpia: { privateKey: ethWallet.privateKey },
+    uid
+  };
+
+  const userDocRef = userRef.doc(uid);
+  const walletDocRef = walletRef.doc(uid);
+  const walletPrivDocRef = walletPrivRef.doc(uid);
+
+  batch.set(userDocRef, userProfile);
+  batch.set(walletDocRef, wallet);
+  batch.set(walletPrivDocRef, walletPrivate);
+
+  return batch.commit();
+}); */
--- index.js
+++ index.js
@@ -8,8 +8,13 @@ const {
   updateTimestamp
 } = require("./users/users");
 
+const Web3 = require("web3");
+const keccak_256 = require("js-sha3").keccak256;
+const config = require("config");
+
 const admin = require("firebase-admin");
 const FieldValue = admin.firestore.FieldValue;
+const DocumentID = admin.firestore.FieldPath.documentId();
 const serviceAccount = require("../firebase-service-account-creds/serviceAccount.json");
 
 admin.initializeApp({
@@ -36,5 +41,68 @@ const bonus = 5000;
 //allUsers(userRef);
 //addReferral(db, user, referred, bonus);
 //getTransactionWithHash();
-
 //addTransaction();
+
+/* walletsRef
+  .limit(6)
+  .get()
+  .then(allUsers => {
+    let index = 1;
+    allUsers.forEach(doc => {
+      console.log(index + ") " + doc.id);
+      index++;
+    });
+  });
+ */
+
+const donContract = "0xe69968dd1913f135f3b28ed81d9a02368204bd66";
+const projectIdEndPt = `/${config.get("project_id")}`;
+const projectSecret = `${config.get("project_secret")}`;
+const infuraEndPt = Axios.create({
+  baseURL: "https://mainnet.infura.io/v3",
+  headers: { "Content-type": "application/json" },
+  auth: { username: "", password: projectSecret }
+});
+
+const smallAcct = "0x57f1fc145b4a5b1efa8066e43eff31fbf68deebe";
+const bigAcct = "0xaEeB5db2Aa3EDcD699BE99293d0e36541E3D52E1";
+
+// Get ETH balance
+infuraEndPt
+  .post(projectIdEndPt, {
+    jsonrpc: "2.0",
+    method: "eth_getBalance",
+    params: [bigAcct, "latest"],
+    id: 1
+  })
+  .then(res => {
+    var balance = Web3.utils.fromWei(res.data.result, "ether");
+    console.log(balance + " ETH");
+  })
+  .catch(ex => console.log(ex.message));
+
+// Get DONPIA balance
+const data =
+  "0x" +
+  keccak_256.hex("balanceOf(address)").substr(0, 8) +
+  "000000000000000000000000" +
+  bigAcct.substr(2);
+
+infuraEndPt
+  .post(projectIdEndPt, {
+    jsonrpc: "2.0",
+    method: "eth_call",
+    params: [
+      {
+        to: donContract,
+        data: data
+      },
+      "latest"
+    ],
+    id: 1
+  })
+  .then(res => {
+    var balance = Web3.utils.fromWei(res.data.result, "ether");
+    console.log(balance + " DON");
+  })
+  .catch(ex => console.log(ex.message));
