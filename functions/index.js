99bc6951928432b199b4349cb0e2f9b432d0d7ca
Andy <aakawade9@gmail.com>
2019-11-22 16:33:21 -0800

    environment variables configuration

--- functions/index.js
+++ functions/index.js
@@ -62,8 +62,11 @@ app.post("/buy-sell-crypto/sell/:uid", (req, res) => {
 
 app.get("/hello", (req, res) => {
   console.log("Hello World!");
+  console.log("Proejct ID", functions.config().infura_project.id);
   console.log(req);
-  res.send("Hello World!");
+  res.send(
+    `Hello World! with ProjectID: ${functions.config().infura_project.id}`
+  );
 });
 
 exports.widgets = functions.https.onRequest(app);
